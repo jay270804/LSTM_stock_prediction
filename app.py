133ca0da944f0608afc1b89b0d832678f1fab6b5
jay270804 <jaypatel270804@gmail.com>
2024-04-05 22:15:31 +0530

    Commiting after adding ticks and increasing testing data.

--- app.py
+++ app.py
@@ -29,26 +29,36 @@ st.write(df.describe())
 st.subheader('Closing Price vs Time Chart')
 fig=plt.figure(figsize=(12,6))
 plt.plot(df.Close)
+plt.xlabel('Time')
+plt.ylabel('Price')
 st.pyplot(fig)
 
 st.subheader('Closing Price vs Time Chart with 100 Days Moving Average')
 MA100=df.Close.rolling(100).mean()
 fig=plt.figure(figsize=(12,6))
-plt.plot(df.Close)
-plt.plot(MA100,'r')
+plt.plot(df.Close,label='Closing Price')
+plt.plot(MA100,'r',label='100 Day Moving Avg')
+plt.xlabel('Time')
+plt.ylabel('Price')
+plt.legend()
 st.pyplot(fig)
 
 st.subheader('Closing Price vs Time Chart with 100 & 200 Days Moving Average')
 MA100=df.Close.rolling(100).mean()
 MA200=df.Close.rolling(200).mean()
 fig=plt.figure(figsize=(12,6))
-plt.plot(df.Close)
-plt.plot(MA100,'r')
-plt.plot(MA200,'g')
+plt.plot(df.Close,label='Closing Price')
+plt.plot(MA100,'r',label='100 Day Moving Avg')
+plt.plot(MA200,'g',label='200 Day Moving Avg')
+plt.xlabel('Time')
+plt.ylabel('Price')
+plt.legend()
 st.pyplot(fig)
 
-train_data=pd.DataFrame(df['Close'][0:int(len(df)*0.70)])
-test_data=pd.DataFrame(df['Close'][int(len(df)*0.70):int(len(df))])
+# train_data=pd.DataFrame(df['Close'][0:int(len(df)*0.70)])
+# test_data=pd.DataFrame(df['Close'][int(len(df)*0.70):int(len(df))])
+train_data=pd.DataFrame(df['Close'][0:101])
+test_data=pd.DataFrame(df['Close'][101:int(len(df))])
 
 #converting this sequential data into 01 values.
 scaler=MinMaxScaler(feature_range=(0,1))
@@ -85,14 +95,13 @@ y_predicted=model.predict(x_test)
 scaler_reverse=scaler.scale_
 
 #converting scaled data 01 back to original values from dataset.
-
 scale_fact=1/scaler_reverse[0]
-
 y_predicted,y_test=(y_predicted*scale_fact),(y_test*scale_fact)
 
 #finally drawing a graph with actual outcome and my bot's predictions.
 st.subheader('Predicted Prices vs Original Prices')
 fig2=plt.figure(figsize=(12,6))
+plt.xticks(np.arange(0, 4000, step=500), labels=['2010','2012','2014','2016','2018','2020','2022','2024'])
 plt.plot(y_test,'g',label='Original Price')
 plt.plot(y_predicted,'r',label='Predicted Price')
 plt.xlabel('Time')
